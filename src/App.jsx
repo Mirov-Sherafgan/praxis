a75755991b64d8674aa95b6736a0a4d2376f6de3
Sheravgan Mirov <mirov.she@gmail.com>
2023-02-03 02:28:23 +0500

    feat: add first modal content

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import {useEffect, useState} from "react";
 import Main from "./components/Main/Main.jsx";
 
+
 function App() {
   const [width, height] = useWindowSize()
   const [modalShow, setModalShow] = useState(true)
 
   useEffect(() => {
     setTimeout(() => {
-      setModalShow(false)
+      // setModalShow(false)
     }, 4000)
   }, [])
 
-  if (modalShow) return <div className="w-screen fixed h-screen top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-violet-500 to-fuchsia-500">Modal is open</div>
+  if (modalShow) return <div className="w-screen grid place-items-center fixed h-screen top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-violet-500 to-fuchsia-500">
+    <img src={logo} className="rounded-full w-64 h-64 animate-bounce" alt="logo"/>
+  </div>
 
   if (width > 655) {
     return <div>Извините, приложение доступна только на телефоне</div>
